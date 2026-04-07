dd8eec7048fa9d95dc14b26dd197a1ce3f10dbff
xtsulyts <531xtsulyts@gmail.com>
2026-04-07 10:46:40 -0400

    tp integrador des_app_web

---
+++ Recursos de ejemplo de clase-20260407/Script_BD.sql
@@ -0,0 +1,41 @@
+﻿CREATE TYPE estados_usuarios AS ENUM ('ACTIVO','BAJA');
+CREATE TYPE estados_clientes AS ENUM ('ACTIVO','BAJA');
+CREATE TYPE estados_proyectos AS ENUM ('ACTIVO','FINALIZADO','BAJA');
+CREATE TYPE estados_tareas AS ENUM ('PENDIENTE','FINALIZADA','BAJA');
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL UNIQUE,
+    clave TEXT NOT NULL,
+    estado estados_usuarios NOT NULL
+);
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL UNIQUE,
+    estado estados_clientes NOT NULL
+);
+
+CREATE TABLE proyectos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL UNIQUE,
+    estado estados_proyectos NOT NULL,
+    id_cliente INT,
+    CONSTRAINT fk_proyectos_cliente
+        FOREIGN KEY (id_cliente)
+        REFERENCES clientes (id)
+);
+
+CREATE TABLE tareas (
+    id SERIAL PRIMARY KEY,
+    descripcion TEXT NOT NULL,
+    estado estados_tareas NOT NULL,
+    id_proyecto INT NOT NULL,
+    CONSTRAINT fk_tareas_proyecto
+        FOREIGN KEY (id_proyecto)
+        REFERENCES proyectos (id)
+);
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+insert into usuarios (nombre, clave, estado) values ('usuario', crypt('clave', gen_salt('bf', 10)), 'ACTIVO')
