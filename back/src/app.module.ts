7a15451bcf80bd5e8c5f5cba54ca43b664dbe0bb
xtsulyts <531xtsulyts@gmail.com>
2026-04-25 11:30:40 -0400

    feat: configurar TypeORM con conexión a PostgreSQL

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,18 +1,31 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './modules/auth/auth.module';
-import { GestionModule } from './modules/gestion/gestion.module';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    AuthModule,
-    GestionModule,
-  ],
-  controllers: [],
-  providers: [],
-  exports: [],
-})
-export class AppModule {}
+import { Module } from '@nestjs/common';                                                                            
+  import { ConfigModule, ConfigService } from '@nestjs/config';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { AuthModule } from './modules/auth/auth.module';                                                            
+  import { GestionModule } from './modules/gestion/gestion.module';
+                                                                                                                      
+  @Module({       
+    imports: [                                                                                                        
+      ConfigModule.forRoot({
+        isGlobal: true,
+      }),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],                                                                                      
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({                                                                     
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),                                                                        
+          username: config.get<string>('DB_USER'),
+          password: config.get<string>('DB_PASSWORD'),                                                                
+          database: config.get<string>('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],                                                            
+          synchronize: false,
+          migrations: [__dirname + '/migrations/*{.ts,.js}'],                                                         
+        }),                                                                                                           
+      }),
+      AuthModule,                                                                                                     
+      GestionModule,
+    ],
+  })
+  export class AppModule {}
