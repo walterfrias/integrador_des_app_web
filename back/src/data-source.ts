143d9f14eb6d3e1a0e5c497c415fc92fb43f6a97
xtsulyts <531xtsulyts@gmail.com>
2026-04-25 11:54:32 -0400

    feat: configurar data source y scripts de migraciones TypeORM

---
+++ back/src/data-source.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
