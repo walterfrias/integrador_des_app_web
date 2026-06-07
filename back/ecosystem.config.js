1392100daa2c6d0a9000a683f3d3085eeb91a926
Gustavo Ortiz <92409193+Gustaf02@users.noreply.github.com>
2026-06-07 12:41:24 -0300

    Subo cambios a mi rama

--- back/ecosystem.config.js
+++ back/ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
                 DB_HOST: 'localhost',
                 DB_PORT: 5432,
                 DB_USERNAME: 'postgres',
-                DB_PASSWORD: 'santiagobd',
-                DB_NAME: 'integradorDawBD',
+                DB_PASSWORD: 'admin',
+                DB_NAME: 'integrador_web',
                 DB_LOGGING: 'false',
                 SWAGGER_HABILITADO: false,
                 JWT_SECRET: "gtT0zY6&5Sx%7c29x&O4@^@73D&uz^xQ"
